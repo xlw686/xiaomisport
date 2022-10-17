4dd155bd2abb3e02fcdd95b1ff6537c640daf618
konghuanqing <57086373+xlw686@users.noreply.github.com>
2022-10-17 19:39:45 +0800

    Update main.py
    修改随机步数

--- main.py
+++ main.py
@@ -88,7 +88,8 @@ def main(_user, _passwd, _step):
 
     if _step == '':
         print("已设置为随机步数（10000-19999）")
-        _step = str(random.randint(10000, 19999))
+#         _step = str(random.randint(10000, 19999))
+        _step = str(random.randint(6666, 15666))
     login_token, userid = login(_user, password)
     if login_token == 0:
         print("登陆失败！")
